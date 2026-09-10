1c4daaa804e0576ca614b202e9ffefe18d0d9a9b
zaidrauf17 <156211053+zaidrauf17@users.noreply.github.com>
2026-09-10 18:35:18 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-blue-400 flex justify-center items-center">
+      <h1 className="text-white">THis is Home Page</h1>
+    </div>
+  );
+};
+
+export default App;
